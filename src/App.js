12e6acdce729fd622a031262f851239b1923655e
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2025-01-08 21:38:31 +1100

    Restructured API calls for improved readability and scalability

--- src/App.js
+++ src/App.js
@@ -9,42 +9,83 @@ import WeatherBox from "./components/WeatherBox";
 // 5. Loading spinner.
 
 function App() {
+  const cities = ["Seoul", "Berlin", "New York"];
+
   const [weather, setWeather] = useState();
   const [forecast, setForecast] = useState();
+  const [city, setCity] = useState("");
+
+  const API_KEY = "38a63e7b6d9d409e80c797942ae598c0";
+
+  const fetchData = async (url) => {
+    try {
+      let response = await fetch(url);
+      let weatherData = await response.json();
+      return weatherData;
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
-      getCurrentWeather(lat, lon);
-      getWeatherForecast(lat, lon);
+      getWeatherByCurrentLocation(lat, lon);
     });
   };
 
-  const getCurrentWeather = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=38a63e7b6d9d409e80c797942ae598c0`;
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log("data>>>", data);
-    setWeather(data);
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    try {
+      let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+      let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+      const weatherData = fetchData(weatherUrl);
+      const forecastData = fetchData(forecastUrl);
+
+      setWeather(weatherData);
+      if (forecastData?.list) {
+        setForecast(forecastData.list.filter((item, index) => item.dt_txt.includes()));
+      }
+      setCity('');
+
+      console.log("Weatherdata>>>", weatherData);
+      console.log("Forecastdata>>>", forecastData);
+    } catch (error) {
+      alert(error.message);
+    }
   };
 
-  const getWeatherForecast = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=38a63e7b6d9d409e80c797942ae598c0`;
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log("forecast>>>", data);
-    setForecast(data);
+  const getWeatherByCityName = async (city) => {
+    try {
+      let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`;
+      let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_key}`;
+
+      const weatherDataByCity = fetchData(weatherUrl);
+      const forcastDataByCity = fetchData(forecastUrl);
+
+      setWeather(weatherDataByCity);
+      setForecast(forcastDataByCity);
+
+      console.log("CityWeatherdata>>>", weatherDataByCity);
+      console.log("CityForecastdata>>>", forcastDataByCity);
+    } catch (error) {
+      alert(error.message);
+    }
   };
 
   useEffect(() => {
-    getCurrentLocation();
-    console.log("first");
-  }, []);
+    if (city === '') {
+      getCurrentLocation();
+    } else {
+      getWeatherByCityName(city);
+    }
+  }, [city]);
 
   return (
     <div>
       <WeatherBox weather={weather} forecast={forecast} />
+      <CityButton cities={cities} setCity={setCity} />
     </div>
   );
 }
---
+++ src/components/CityButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CityButton.style.css';
+
+const CityButton = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CityButton
